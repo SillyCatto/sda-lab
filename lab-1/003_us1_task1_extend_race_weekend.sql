0b8ae856e7bea3ee1284530c88f4b8805e201616
Dr-Lepic <mbmahbub007@gmail.com>
2026-05-04 11:50:26 +0600

    Create 003_us1_task1_extend_race_weekend.sql

---
+++ lab-1/003_us1_task1_extend_race_weekend.sql
@@ -0,0 +1,20 @@
+-- 003_us1_task1_extend_race_weekend.sql
+
+-- MySQL does not support "IF NOT EXISTS" for ADD COLUMN in ALTER TABLE statements.
+-- will throw a "Duplicate column name" error if re-run
+
+ALTER TABLE race_weekend 
+ADD COLUMN has_sprint BOOLEAN DEFAULT FALSE;
+
+ALTER TABLE race_weekend 
+ADD COLUMN sprint_date TIMESTAMP NULL;
+
+-- verification
+SELECT * FROM race_weekend;
+
+-- Changelog
+
+INSERT INTO change_log (applied_at, created_by, script_name, script_details) 
+VALUES ( CURRENT_TIMESTAMP, CURRENT_USER(), '003_us1_task1_extend_race_weekend.sql', 
+    'Added has_sprint and sprint_date to race_weekend for User Story 1 Task 1'
+);
